323c7e1d479188b56db9b9f36a01e1a05ea3cfac
hydaly ouanesavanh <ouanehyd000@gmail.com>
2022-10-26 19:32:37 -0700

    added subtraction function and const number3 and number4.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,18 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-// user interface logic
-const number1 = parseInt(prompt("Enter a number"));
-const number2 = parseInt(prompt("Enter another number"));
+function subtract(number3, number4) {
+  return number3 - number4;
+}
 
-window.alert(add(number1, number2));
+// user interface logic
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+const number1 = parseInt(prompt("Enter a number to add"));
+const number2 = parseInt(prompt("Enter another number add"));
+window.alert(saySomething("The sum is " + add(number1, number2) + "!"));
+const number3 = parseInt(prompt("Enter a number to subtract"));
+const number4 = parseInt(prompt("Enter another number to subtract"))
+
+window.alert(subtract(number3, number4));
