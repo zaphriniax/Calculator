78b6e760f3103d07ac8a0970ba47e745928d7b44
hydaly ouanesavanh <ouanehyd000@gmail.com>
2022-10-26 19:10:55 -0700

    added prompts for arguments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
-function add(number1,number2); {
+// business logic
+function add(number1, number2) {
   return number1 + number2;
 }
+
+// user interface logic
+const number1 = parseInt(prompt("Enter a number"));
+const number2 = parseInt(prompt("Enter another number"));
+
+window.alert(add(number1, number2));
