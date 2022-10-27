4b46be2703c02280106e2cd3945f1a448b7c35f9
hydaly ouanesavanh <ouanehyd000@gmail.com>
2022-10-26 19:47:20 -0700

    :added const for number3-8 and inserted alerts for all functions.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ function subtract(number3, number4) {
 function multiply(number5, number6) {
   return number5 * number6;
 }
+
+function divide(number7, number8) {
+  return number7 / number8;
+}
 // user interface logic
 function saySomething(whatToSay) {
   window.alert(whatToSay);
@@ -19,6 +23,11 @@ const number2 = parseInt(prompt("Enter another number add"));
 window.alert(saySomething("The sum is " + add(number1, number2) + "!"));
 const number3 = parseInt(prompt("Enter a number to subtract"));
 const number4 = parseInt(prompt("Enter another number to subtract"));
+window.alert(saySomething("The difference is " + subtract(number3, number4) + "!"));
 const number5 = parseInt(prompt("Enter a number to multiply"));
 const number6 = parseInt(prompt("Enter another number to multiply"));
-window.alert(subtract(number3, number4));
+window.alert(saySomething("The product is " + multiply(number5, number6) + "!"));
+const number7 = parseInt(prompt("Enter a number to divide"));
+const number8 = parseInt(prompt("Enter another number to divide"));
+window.alert(saySomething("The quotient is " + divide(number7, number8) + "!"));
+
